31d45144658bc9611b5e3f1b4752ea3365139287
uluturhandilara <dilarauluturhan@gmail.com>
2026-03-01 17:50:06 +0300

    project-setup

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Serif, Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const ibmPlexSerif = IBM_Plex_Serif({
+  variable: "--font-ibm-plex-serif",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+});
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Bookly",
+  description:
+    "Transform your books into interactive AI conversations. Upload PDFs, and chat with your books using voice.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${ibmPlexSerif.variable} ${monaSans.variable} relative font-sans antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold underline">Bookly</h1>
+    </div>
+  );
+}
